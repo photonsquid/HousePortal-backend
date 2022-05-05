4ae04fd0dd275b669dff2ee4ecc5ab0df86c3276
Sébastien PONT <seba12.04@orange.fr>
2022-05-05 12:50:57 +0200

    refactor: :technologist: implement wildfly in docker

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM quay.io/wildfly/wildfly-centos7
-
-ADD lib/build/libs/houseportal-backend.war /opt/wildfly/standalone/deployments/
---
+++ src/main/java/fr/photonsquid/houseportal/RestApplication.java
@@ -0,0 +1,8 @@
+package fr.photonsquid.houseportal;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+}
